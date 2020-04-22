b48ea131f53215c1ac05d7f8e21a4b30ee71b6c7
carneirofc <claudiofcarneiro@hotmail.com>
2020-04-22 15:53:42 -0300

    Setup files

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+requirements = None
+with open("requirements.txt", r) as _f:
+    requirements = _f.readlines()
+
 setup(
     name="application",
     packages=["application"],
     include_package_data=True,
-    install_requires=[
-        "Flask==1.1.1",
-        "pandas==1.0.3",
-        "requests==2.23.0",
-        "xlrd==1.2.0",
-    ],
+    install_requires=requirements,
 )
